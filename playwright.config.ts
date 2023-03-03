0ce3143197876a9d6d21e3dcaa2f609c10638513
Michael Zuercher <michzuerch@gmail.com>
2023-03-03 16:42:20 +0100

    new versions, webkit tests not working

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,14 +23,14 @@ const config: PlaywrightTestConfig = {
     screenshot: 'on',
   },
   projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'chromium',
+    //   use: { ...devices['Desktop Chrome'] },
+    // },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
