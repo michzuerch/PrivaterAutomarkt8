6b367e816402f2ad415017e3820c985da72a49a8
Michael Zuercher <michzuerch@gmail.com>
2023-03-04 17:16:08 +0100

    Prototype tests with  docker

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,7 +13,7 @@ const config: PlaywrightTestConfig = {
     : [['html', { outputFolder: 'test-report' }], ['list']],
   use: {
     actionTimeout: 30 * 1000,
-    headless: false,
+    headless: true,
     locale: 'de-DE',
     baseURL: 'http://localhost:3000',
     viewport: { width: 1920, height: 1080 },
@@ -23,25 +23,24 @@ const config: PlaywrightTestConfig = {
     screenshot: 'on',
   },
   projects: [
-    // {
-    //   name: 'chromium',
-    //   use: { ...devices['Desktop Chrome'] },
-    // },
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
   ],
-
-  webServer: {
+ webServer: {
     command: 'npm run dev',
-    url: 'http://localhost:3000',
-    timeout: 30 * 1000,
-    reuseExistingServer: true,
+    url: 'http://localhost:3000/',
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
   },
 };
 
