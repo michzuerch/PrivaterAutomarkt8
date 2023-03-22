40f5f3d78ee3f8b5e07c4de151bb5796072eda15
Michael Zuercher <michzuerch@gmail.com>
2023-03-22 10:37:46 +0100

    Add svelte, rename prettier to cjs

---
+++ src/components/widgets/Gallery.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { onMount } from 'svelte';
+  import PhotoSwipeLightbox from 'photoswipe/lightbox';
+  import 'photoswipe/style.css';
+  export let galleryID;
+  export let images;
+
+  onMount(() => {
+    let lightbox = new PhotoSwipeLightbox({
+      gallery: '#' + galleryID,
+      children: 'a',
+      pswpModule: () => import('photoswipe'),
+    });
+    lightbox.init();
+  });
+</script>
+
+<div class="pswp-gallery" id={galleryID}>
+  {#each images as image}
+    <a
+      href={image.largeURL}
+      data-pswp-width={image.width}
+      data-pswp-height={image.height}
+      target="_blank"
+      rel="noreferrer"
+    >
+      <img src={image.thumbnailURL} alt="" />
+    </a>
+  {/each}
+</div>
